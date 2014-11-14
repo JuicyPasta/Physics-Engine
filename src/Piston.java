b59bbc58b93eb928963799624c389ad231727e8a
JuicyPasta <jackson.darrow@gmail.com>
2014-11-13 23:49:18 -0800

    structure changes

--- src/Piston.java
+++ src/Piston.java
@@ -21,10 +21,26 @@ public class Piston {
         this.eng=eng;
         this.density = density;
     }
-    public void update(){
-        
+    public Piston (double x, double y, double vx, double vy, double ax, double ay, double rx, int density, Shape shape){
+        this(null,x,y,vx,vy,ax,ay,rx,density,shape);
     }
-
+    public boolean isBound(){
+        return eng != null;
+    }
+    public void update(){
+        if (isBound()){
+            x += vx;
+            y += vy;
+            vx += ax;
+            vy += vy;
+            //Handle the next move with eng.physics.XXX();
+        }else{
+            System.out.println("this piston is not bound to an engine :(");
+        }
+    }
+    public void bindEngine(Engine eng){
+        this.eng=eng;
+    }
     public double mass(){
 
         return shape.area()*density;
